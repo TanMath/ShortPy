e3fe38106fcdc5f30666a02301bffa317aa60e18
TanMath <TanMath@users.noreply.github.com>
2015-11-12 21:24:37 -0800

    Create main.py
    Added two functions, input and function

---
+++ main.py
@@ -0,0 +1,11 @@
+import math
+
+program = input()
+
+def input():
+    return "input()"
+def function(name,arg,code):
+    variables=[q,r,s,t,u,v,w,x,y,z]
+    return "def "+"+name+"("+ #add variables
+ 
+    
