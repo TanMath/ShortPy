58f12124a1b2bc3946283a456c50250816a3b016
TanMath <TanMath@users.noreply.github.com>
2015-11-13 20:44:50 -0800

    Update and rename main.py to shortpy.py

---
+++ shortpy.py
@@ -0,0 +1,21 @@
+import math
+
+program = input()
+
+def input():
+    return "input()"
+def function(name,arg,code):
+    variables='qrstuvwxyz'
+    return "def "+"+name+"("+ ','.join(variables[0:n])+"):\n\t"+code
+def print(statement):
+    return "print"+statement
+
+programfile=open("program.py", "w+")
+
+for i in program:
+    if i == "I":
+        input = input()
+        program.write(input)
+    elif i == "D :
+         #Solve problem with functions - D is for defining a function, but need to find next characters for function definition.
+    
