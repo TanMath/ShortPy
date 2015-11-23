407c4d8a923a5ec1cb8aef830734b915f485e812
TanMath <TanMath@users.noreply.github.com>
2015-11-23 13:00:44 -0800

    Update shortpy.py

--- shortpy.py
+++ shortpy.py
@@ -2,20 +2,35 @@ import math
 
 program = input()
 
-def input():
+printlist=[]
+
+def tinput():
     return "input()"
 def function(name,arg,code):
     variables='qrstuvwxyz'
     return "def "+name+"("+ ','.join(variables[0:n])+"):\n\t"+code
-def print(statement):
-    return "print"+statement
+def print(i):
+    if program[i+1]='"':
+        for i,j in enumerate(program)
+            j #work on this
+        
+    
+funcdict={}
 
 programfile=open("program.py", "w+")
 
 for i in program:
+    if printlist.find(i):
+        pass
     if i == "I":
-        input = input()
+        input = tinput()
         program.write(input)
-    elif i == "D :
+    elif i == "D" :
          #Solve problem with functions - D is for defining a function, but need to find next characters for function definition.
+        funcdict[D]=[i]
+    elif i=="p":
+        print=print(i)
+    
+        
+    
     
